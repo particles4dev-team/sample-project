b4c9fd2076faa2b41587b3477c32eea1e9cdcb5d
Particle4Dev <steve@p4d-Vostro-270.(none)>
2014-09-30 01:50:52 +0700

    update

--- project/lib/utils/util.js
+++ project/lib/utils/util.js
@@ -4,11 +4,17 @@
 
 /**
  * Random array or string
- * 
  */
 rand = _.bind(Random.choice, Random);
 emailVerified = function (user) {
     return _.some(user.emails, function (email) {
         return email.verified;
     });
+};
+
+/**
+ * Mongo
+ */
+isCursor = function (c) {
+    return c && c.observe;
 };
