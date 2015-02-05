abad05699437edb91a6ca1e06e95e5e85fb25a11
Le Hoang <particle4dev@gmail.com>
2015-02-06 03:25:57 +0700

    Update router.js

--- project/lib/core/router.js
+++ project/lib/core/router.js
@@ -1,17 +1,19 @@
 /**
  * Helpers router
  */
-Router.getCurrentRouterName = function() {
-    var routeName, router;
-    router = Router.current();
-    if (!router) {
-        return;
-    }
-    routeName = router.route.getName();
-    return routeName;
-};
+if(Router){
+    Router.getCurrentRouterName = function() {
+        var routeName, router;
+        router = Router.current();
+        if (!router) {
+            return;
+        }
+        routeName = router.route.getName();
+        return routeName;
+    };
 
-Router.reload = function(){
-    var current = Router.current();
-    Router.go(current.route.name, current.params);
-};
+    Router.reload = function(){
+        var current = Router.current();
+        Router.go(current.route.name, current.params);
+    };
+}
