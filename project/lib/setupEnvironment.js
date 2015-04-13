d47c5e0f2ffa1838260c3a27fbb8746f99b3b8e4
Le Hoang <particle4dev@gmail.com>
2015-04-13 21:54:59 +0700

    Create setupEnvironment.js

---
+++ project/lib/setupEnvironment.js
@@ -0,0 +1,9 @@
+if(SYSTEMS_INFO.ENVIRONMENT == SC.DEVELOPMENT) {
+    // development mod
+}
+else if(SYSTEMS_INFO.ENVIRONMENT == SC.PRODUCTION) {
+    // production mod
+}
+else if(SYSTEMS_INFO.ENVIRONMENT == SC.TESTING) {
+    // testing mod
+}
