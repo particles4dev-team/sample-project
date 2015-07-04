82b27689521a3162f1e1064c8640c389b01c8f21
Le Hoang <particle4dev@gmail.com>
2015-07-04 09:21:39 +0700

    Update main.js

--- project/server/main.js
+++ project/server/main.js
@@ -1,7 +1,7 @@
 Meteor.startup(function(){
     // fixtures
     var fixturesModule = APP.namespace('app.module.FIXTURES');
-
+    fixturesModule.reset();
     fixturesModule.setIndex();
     fixturesModule.init();
 });
